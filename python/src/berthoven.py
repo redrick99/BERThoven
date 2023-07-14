4e57d544cfcfbe143c4afa0fbe7039d6c4014e46
redrick99 <10628467@polimi.it>
2023-07-14 23:56:14 +0200

    Uploaded files for technical evaluation. Updated berthoven.py.

--- python/src/berthoven.py
+++ python/src/berthoven.py
@@ -1,4 +1,5 @@
 import os
+import time
 import pandas as pd
 import torch
 import random
@@ -18,14 +19,11 @@ from music21 import converter, instrument, note, chord
 torch.manual_seed(41)
 
 
-def get_notes(path_to_resources_folder: str, octave_aware=True):
+def get_notes(path_to_resources_folder: str):
     """ Get all the notes and chords from the midi files in the ./midi_songs directory """
     notes = []
 
-    if octave_aware:
-        path_to_notes_file = os.path.join(path_to_resources_folder, "data", "notes")
-    else:
-        path_to_notes_file = os.path.join(path_to_resources_folder, "data", "notes_no_octave_no_chords")
+    path_to_notes_file = os.path.join(path_to_resources_folder, "data", "notes_no_octave_no_chords")
 
     path_to_midi_files = os.path.join(path_to_resources_folder, "midi_songs")
 
@@ -34,7 +32,7 @@ def get_notes(path_to_resources_folder: str, octave_aware=True):
             notes = pickle.load(filepath)
             return notes
 
-    for file in glob.glob(path_to_midi_files+"/*.mid"):
+    for file in glob.glob(path_to_midi_files + "/*.mid"):
         midi = converter.parse(file)
 
         print("Parsing %s" % file)
@@ -49,10 +47,7 @@ def get_notes(path_to_resources_folder: str, octave_aware=True):
 
         for element in notes_to_parse:
             if isinstance(element, note.Note):
-                if octave_aware:
-                    notes.append(str(element.pitch))
-                else:
-                    notes.append(str(element.pitch.pitchClass))
+                notes.append(str(element.pitch.pitchClass))
             elif isinstance(element, chord.Chord):
                 continue
                 notes.append(str(element.normalOrder[0]))
@@ -111,7 +106,7 @@ def prepare_dataset(notes, in_seq_length, over_sampling=False, shuffle=False, nu
     input = input[:num_of_samples]
     expected_result = expected_result[:num_of_samples]
 
-    #for i in range(12):
+    # for i in range(12):
     #    print(f"Note {i}: {expected_result.count(str(i))}")
 
     if over_sampling:
@@ -120,16 +115,14 @@ def prepare_dataset(notes, in_seq_length, over_sampling=False, shuffle=False, nu
 
     df = pd.DataFrame({'text': input, 'category': expected_result})
     np.random.seed(112)
-    df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42), [int(.8*len(df)), int(.9*len(df))])
+    df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42), [int(.8 * len(df)), int(.9 * len(df))])
     return df_train, df_val, df_test
 
-
-OCTAVE_AWARE = False
 
 src_path = os.path.dirname(__file__)
 resources_path = os.path.join(src_path, "resources")
 
-notes = get_notes(resources_path, OCTAVE_AWARE)
+notes = get_notes(resources_path)
 pitchnames = sorted(set(item for item in notes))
 n_vocab = len(pitchnames)
 note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
@@ -148,7 +141,7 @@ class Dataset(torch.utils.data.Dataset):
     def __init__(self, df):
         self.labels = [labels[label] for label in df['category']]
         self.texts = [tokenizer(text,
-                               padding='max_length', max_length=200, truncation=True,
+                                padding='max_length', max_length=200, truncation=True,
                                 return_tensors="pt") for text in df['text']]
 
     def classes(self):
@@ -183,7 +176,7 @@ class BertClassifier(nn.Module):
         self.relu = nn.ReLU()
 
     def forward(self, input_id, mask):
-        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        _, pooled_output = self.bert(input_ids=input_id, attention_mask=mask, return_dict=False)
         dropout_output = self.dropout(pooled_output)
         linear_output = self.linear(dropout_output)
         final_layer = self.relu(linear_output)
@@ -191,34 +184,55 @@ class BertClassifier(nn.Module):
         return final_layer
 
 
-def train(model, train_data, val_data, learning_rate, epochs, dump=False, early_stopping=True):
+def save_model_checkpoints(epoch: int, model, optimizer, path_to_model_checkpoint):
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+    }, path_to_model_checkpoint + ".pth")
+
+
+def load_model_checkpoints(model, optimizer, path_to_model_checkpoint):
+    checkpoint = torch.load(path_to_model_checkpoint + ".pth")
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    return model, optimizer, epoch
+
+
+def train(model, train_data, val_data, learning_rate, epochs, batch_size: int, path_to_model="", path_to_checkpoint="",
+          early_stopping=True, load=False):
     train, val = Dataset(train_data), Dataset(val_data)
 
-    train_dataloader = torch.utils.data.DataLoader(train, batch_size=2, shuffle=True)
-    val_dataloader = torch.utils.data.DataLoader(val, batch_size=2)
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=False)
+    val_dataloader = torch.utils.data.DataLoader(val, batch_size=batch_size)
 
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
 
     criterion = nn.CrossEntropyLoss()
     optimizer = Adam(model.parameters(), lr=learning_rate)
+    start_epoch = 0
 
     train_loss_per_epoch = []
     train_acc_per_epoch = []
     val_loss_per_epoch = []
     val_acc_per_epoch = []
+    elapsed_time_per_epoch = []
 
     best_val_loss = np.inf
     early_stop_counter = 0
+    epsilon = .001
     patience = 5
 
     if use_cuda:
         model = model.cuda()
         criterion = criterion.cuda()
 
-    for epoch_num in range(epochs):
+    for epoch_num in range(start_epoch, epochs):
         total_acc_train = 0
         total_loss_train = 0
+        s_time = time.time()
 
         for train_input, train_label in tqdm(train_dataloader):
             train_label = train_label.to(device)
@@ -254,33 +268,36 @@ def train(model, train_data, val_data, learning_rate, epochs, dump=False, early_
                 acc = (output.argmax(dim=1) == val_label).sum().item()
                 total_acc_val += acc
 
-        train_loss_per_epoch.append(total_loss_train/len(train_data))
-        train_acc_per_epoch.append(total_acc_train/len(train_data))
-        val_loss_per_epoch.append(total_loss_val/len(val_data))
-        val_acc_per_epoch.append(total_acc_val/len(val_data))
+        elapsed_time_per_epoch.append(time.time() - s_time)
+        train_loss_per_epoch.append(total_loss_train / len(train_data))
+        train_acc_per_epoch.append(total_acc_train / len(train_data))
+        val_loss_per_epoch.append(total_loss_val / len(val_data))
+        val_acc_per_epoch.append(total_acc_val / len(val_data))
 
         print(
             f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} | Train Accuracy: {total_acc_train / len(train_data): .3f} | Val Loss: {total_loss_val / len(val_data): .3f} | Val Accuracy: {total_acc_val / len(val_data): .3f}')
 
-        if early_stopping:
-            if total_loss_val < best_val_loss:
-                best_val_loss = total_loss_val
-                early_stop_counter = 0
-            else:
-                early_stop_counter += 1
+        if total_loss_val < best_val_loss - epsilon:
+            best_val_loss = total_loss_val
+            early_stop_counter = 0
+            if path_to_checkpoint != "":
+                save_model_checkpoints(epochs, model, optimizer, path_to_checkpoint)
+        elif early_stopping:
+            early_stop_counter += 1
 
-            if early_stop_counter >= patience:
-                print("Triggered early stopping!")
-                break
+        if early_stop_counter >= patience:
+            print("Triggered early stopping!")
+            break
 
-    if dump:
+    if path_to_model != "":
         train_dump = pd.DataFrame({
             'train_loss': np.array(train_loss_per_epoch),
             'train_accuracy': np.array(train_acc_per_epoch),
             'validation_loss': np.array(val_loss_per_epoch),
             'validation_accuracy': np.array(val_acc_per_epoch),
+            'time': np.array(elapsed_time_per_epoch),
         })
-        train_dump.to_csv('resources/neural_network/training_data/train_dump.csv')
+        train_dump.to_csv(path_to_model + "_dump.csv")
 
 
 def evaluate(model, test_data):
@@ -292,52 +309,63 @@ def evaluate(model, test_data):
     device = torch.device("cuda" if use_cuda else "cpu")
 
     if use_cuda:
-
         model = model.cuda()
 
     total_acc_test = 0
     with torch.no_grad():
-
         for test_input, test_label in test_dataloader:
+            test_label = test_label.to(device)
+            mask = test_input['attention_mask'].to(device)
+            input_id = test_input['input_ids'].squeeze(1).to(device)
 
-              test_label = test_label.to(device)
-              mask = test_input['attention_mask'].to(device)
-              input_id = test_input['input_ids'].squeeze(1).to(device)
+            output = model(input_id, mask)
 
-              output = model(input_id, mask)
-
-              acc = (output.argmax(dim=1) == test_label).sum().item()
-              total_acc_test += acc
+            acc = (output.argmax(dim=1) == test_label).sum().item()
+            total_acc_test += acc
 
     print(f'Test Accuracy: {total_acc_test / len(test_data): .3f}')
 
 
-def save_torch_model_to_file(model: BertClassifier, test_data, file_path: str, version: int, octave_aware=True):
-  test = Dataset(test_data)
-  test_dataloader = torch.utils.data.DataLoader(test, batch_size=2)
+def save_torch_model_to_file(model: BertClassifier, test_data, path_to_model: str):
+    test = Dataset(test_data)
+    test_dataloader = torch.utils.data.DataLoader(test, batch_size=2)
 
-  model.eval()
-  model = model.to("cpu")
+    model.eval()
+    model = model.to("cpu")
 
-  sample_input_id, sample_label = next(iter(test_dataloader))
+    sample_input_id, sample_label = next(iter(test_dataloader))
 
-  sample_label = sample_label.to("cpu")
-  sample_mask = sample_input_id['attention_mask'].to("cpu")
-  sample_input_id = sample_input_id['input_ids'].squeeze(1).to("cpu")
+    sample_label = sample_label.to("cpu")
+    sample_mask = sample_input_id['attention_mask'].to("cpu")
+    sample_input_id = sample_input_id['input_ids'].squeeze(1).to("cpu")
 
-  traced_script_module = torch.jit.trace(model.cpu(), [sample_input_id, sample_mask])
-  if octave_aware:
-    traced_script_module.save(os.path.join(file_path, "berthoven_model_v"+str(version)+".pt"))
-  else:
-    traced_script_module.save(os.path.join(file_path, "berthoven_balanced_no_chords_v"+str(version)+".pt"))
+    traced_script_module = torch.jit.trace(model.cpu(), [sample_input_id, sample_mask])
+    traced_script_module.save(path_to_model + ".pt")
 
 
-df_train, df_val, df_test = prepare_dataset(notes, 100, over_sampling=True, shuffle=False, num_of_samples=6000)
-EPOCHS = 50
-model = BertClassifier(n_vocab)
+def create_and_train_new_model(r_path, n_vocab, notes, n_samp, epochs, seq_len, lr, batch_size, over_sampling, shuffle,
+                               e_stop):
+    model_name = "a_e" + str(epochs) + "_n" + str(n_samp) + "_i" + str(seq_len) + "_lr" + str(lr) + "_bs" + str(
+        batch_size) + "_os" \
+                 + str(int(over_sampling)) + "_s" + str(int(shuffle))
+    model_path = os.path.join(r_path, "neural_network", "evaluation_models_dumps", model_name)
+    check_path = os.path.join(r_path, "neural_network", "checkpoints", model_name)
+    model = BertClassifier(n_vocab)
+    df_train, df_val, df_test = prepare_dataset(notes, in_seq_length=seq_len, over_sampling=over_sampling,
+                                                shuffle=shuffle, num_of_samples=n_samp)
+
+    train(model, df_train, df_val, LR, EPOCHS, batch_size, path_to_model=model_path, path_to_checkpoint=check_path,
+          early_stopping=e_stop, load=False)
+
+
+NUM_OF_SAMPLES = 10000
+EPOCHS = 20
+IN_SEQ_LENGTH = 100
 LR = 1e-6
+BATCH_SIZE = 16
 
-train(model, df_train, df_val, LR, EPOCHS, dump=True)
+num_samples_array = [100, 1000, 5000]
 
-evaluate(model, df_test)
-save_torch_model_to_file(model, df_test, os.path.join(resources_path, "neural_network", "models"), 7, OCTAVE_AWARE)
+for num_samples in num_samples_array:
+    create_and_train_new_model(resources_path, n_vocab, notes, num_samples, EPOCHS, IN_SEQ_LENGTH, LR, BATCH_SIZE, True, False, False)
+
